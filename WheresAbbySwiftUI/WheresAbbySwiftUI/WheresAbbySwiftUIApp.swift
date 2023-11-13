448d5a5da29f92dfaf4cf437ff3262f1f67f2932
Teryn Milliner <107887744+sgteryn@users.noreply.github.com>
2023-11-13 16:30:23 -0500

    Add files via upload

---
+++ WheresAbbySwiftUI/WheresAbbySwiftUI/WheresAbbySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStoryTemplateApp.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Zoe Cutler on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WheresAbbySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
